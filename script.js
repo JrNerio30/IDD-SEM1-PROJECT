e430f1d2726a83edad0036d3142682cba409db63
Fernando Nerio Jr <91402801+JrNerio30@users.noreply.github.com>
2023-11-29 23:04:43 -0700

    Initial Commit

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+// Saying Hello in the Console
+
+let Fernando = ("")
+let say = "Hello, thank you for having your time to look at my portfolio project"
+  console.log(Fernando + say)
+
+
+
+
 // Getting the current year
 
 const yr = new Date();
@@ -24,25 +33,37 @@ const notify = document.querySelector("#MsgSent");
   // Incrementing Numbers
 
   // defualt number when not clicked yet
-var number = 0
+var number = -1
   // selecting the <p id="thumbsup"><p> 
-var count = document.getElementById('thumbsup');
+var count = document.getElementById('heart');
   // giving "incrementor" variable a function using add.EventListener
 var incrementor = document.getElementById('up');
 // When click, the "number variable shows up and it adds up to one everytime the heart icon is click"
   incrementor.addEventListener('click', () =>{
     number++;
     count.textContent = number.toString();
+    const press = document.querySelector("#up")
+      press.addEventListener('click', () =>{
+        if(number %2 == 0){
+          console.log("Even")
+        }
+        else{
+          console.log("Odd")
+        }
+      })
   });
 
-  // for loops practise with names
-const names = ["Kyle", "Kevin", "Kent", "Kyrie"];
+// incrementing button even or Even
 
+  // for loops practise with My favorite cars of all time
+const cars = ["Toyota Trueno AE86,","Range Rover Sport,", "Honda NSX 1991,","Mazda RX7"];
+  let fav = "My favorite Cars: "
   let txt = "";
-  for(let i = 0; i < names.length; i++) {
-    txt += names[i] + " ";
+  for(let i = 0; i < cars.length; i++) {
+    txt += cars[i] + " ";
   }
-console.log(txt)
+
+console.log(fav + txt)
 
 // for loops with numbers
 // selecting the ordered list id "numbers"
