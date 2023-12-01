842e86978afd8ab66637a9fe1cd5701bbe358800
Fernando Nerio Jr <91402801+JrNerio30@users.noreply.github.com>
2023-12-01 09:54:28 -0700

    Initial Commit

--- script.js
+++ script.js
@@ -1,89 +1,89 @@
-// Saying Hello in the Console
-
-let Fernando = ("")
-let say = "Hello, thank you for having your time to look at my portfolio project"
+let Fernando = ("Hello")
+let say = ", thank you for having your time to look at my portfolio project"
   console.log(Fernando + say)
 
 
 
-
-// Getting the current year
-
-const yr = new Date();
-  let year = yr.getFullYear();
-    document.getElementById("year").outerHTML = year;
+// Current Year
+let yr = new Date()
+  let year = yr.getFullYear()
+    document.getElementById("year").outerHTML = year
 
 
-
-const sent = document.querySelector("#MsgSent");
+// Mouse Hover - change background color
+const sent = document.querySelector("#MsgSent")
   sent.addEventListener("mouseover", () =>{
-    sent.style.backgroundColor = "#9e3641";
+    sent.style.backgroundColor = "#9e3641"
       if(sent.addEventListener("mouseout", () =>{ 
-        sent.style.backgroundColor = "";
+        sent.style.backgroundColor = ""
       })){
   }});
 
-const element = document.getElementById("MsgSent");
+// Mouse Hover - Changes text 
+const element = document.getElementById("MsgSent")
   element.onmouseover = () =>{
-    element.innerText = "Send it";
+    element.innerText = "Send it"
 
   element.onmouseout = () =>{
-    element.innerText = "Send Message";
+    element.innerText = "Send Message"
   }}
 
 
-
-const notify = document.querySelector("#MsgSent");
-  let me = ("Your message has been sent. Thank you!");
+// When click, alert pops up
+const notify = document.querySelector("#MsgSent")
+  let me = ("Your message has been sent. Thank you!")
   notify.addEventListener("click",() =>{
     alert(me)
   })
 
-
-let number = -1
-let count = document.getElementById('heart');
-let incrementor = document.getElementById('up');
+// When the heart icon is click, the number goes up by one
+let number = 0
+let count = document.getElementById('heart')
+let incrementor = document.getElementById('up')
   incrementor.addEventListener('click', () =>{
-    number++;
-    count.textContent = number.toString();
+    number++
+    count.textContent = number.toString()
+    // changes color when the heart icon is click
     if(number % 2 ==0){
-      heart.classList.add('Even');
-      heart.classList.remove('Odd');
+      heart.classList.add('Even')
+      heart.classList.remove('Odd')
     }
     else{
-      heart.classList.add('Odd');
-      heart.classList.remove('Even');
+      heart.classList.add('Odd')
+      heart.classList.remove('Even')
     }
-  });
+  })
 
-
-
-const cars = ["Toyota Trueno AE86,","Range Rover Sport,", "Honda NSX 1991,","Mazda RX7"];
+// console message - my favorite cars
+const cars = ["Toyota Trueno AE86,","Range Rover Sport,", "Honda NSX 1991,","Mazda RX7"]
   let fav = "My favorite Cars: "
-  let txt = "";
+  let txt = ""
   for(let i = 0; i < cars.length; i++) {
-    txt += cars[i] + " ";
+    txt += cars[i] + " "
   }
 
+// 1 - 100 for loops with even or odd
 console.log(fav + txt)
 
-   const numberlist = document.getElementById("numbers");
+   const numberlist = document.getElementById("numbers")
 
     for(let i = 1; i <= 100; i++){
 
-      const listitems = document.createElement("li");
+      const listitems = document.createElement("li")
 
-      const even = i % 2 == 0;
+      const even = i % 2 == 0
 
-      listitems.innerHTML = even ? "Even" : "Odd";
+      listitems.innerHTML = even ? "Even" : "Odd"
 
-      numberlist.appendChild(listitems);
+      numberlist.appendChild(listitems)
 
-      if(even) listitems.classList.add("even");
-        else listitems.classList.add("odd");
+      if(even) listitems.classList.add("even")
+        else listitems.classList.add("odd")
     }
   
 
+
+  
 
 
 
